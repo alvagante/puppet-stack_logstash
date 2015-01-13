d3f86a41955af17803824d4d4e806253becae23a
Alessandro Franceschi <al@lab42.it>
2015-01-13 18:15:34 +0000

    Updated logstash default config, first sample templates with beaver

--- manifests/log.pp
+++ manifests/log.pp
@@ -1,8 +1,11 @@
 define stack_logstash::log (
-  $log_path          = $title,
-  $rsyslog_template  = 'stack_logstash/logs/rsyslog.conf.erb',
-  $logstash_template = 'stack_logstash/logs/logstash.conf.erb',
-  $options_hash      = { },
+  $log_path                  = $title,
+  $log_type                  = 'syslog',
+  $log_tags                  = '',
+  $shipper_template          = '',
+  $logstash_filter_template  = 'stack_logstash/logs/logstash.conf.erb',
+  $kibana_dashboard_template = '',
+  $options_hash              = { },
 ) {
 
 
